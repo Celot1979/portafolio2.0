e78aef3412186c2ef485494056b7706fedfea267
Celot1979 <zata_k@hotmail.com>
2025-07-29 12:10:23 +0200

    feat: Configurar adaptador Vercel para Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless'; // Importar el adaptador
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server', // Indicar que el output es para un servidor
+  adapter: vercel(), // Usar el adaptador de Vercel
+});
